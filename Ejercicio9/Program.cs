342a56555151183b124f32bbeab3c06d174ce97a
Gala <galamagdalena@hotmail.com>
2020-03-24 22:32:42 -0300

    Ejercicio9

---
+++ Ejercicio9/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            string[] posibles = { "Gala", "Carlos", "Juan", "Silvio", "Suyai"};
+
+            Console.WriteLine("Ingrese su nombre: ");
+                nombre = Console.ReadLine();
+
+            if (posibles.Contains(nombre)) {
+
+                Console.WriteLine("Hola " + nombre);
+
+            } else
+            {
+                Console.WriteLine("No te conozco");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
